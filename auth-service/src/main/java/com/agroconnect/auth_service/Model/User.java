d9eb7781a0d6e4077fce4d15d1daaa33e9a875e0
kavinkumars21 <kavinkumars218@gmail.com>
2025-05-06 09:28:40 +0530

    updated auth service

--- auth-service/src/main/java/com/agroconnect/auth_service/Model/User.java
+++ auth-service/src/main/java/com/agroconnect/auth_service/Model/User.java
@@ -1,11 +1,18 @@
 package com.agroconnect.auth_service.Model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @Document(collection = "users")
 public class User {
@@ -29,94 +36,11 @@ public class User {
 
     private Set<String> roles = new HashSet<>();
 
-    public User() {
-    }
-
     public User(String userName, String password, String email, String phoneNumber, Set<String> roles) {
         this.userName = userName;
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.roles = roles;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public boolean isExpired() {
-        return isExpired;
-    }
-
-    public void setExpired(boolean expired) {
-        isExpired = expired;
-    }
-
-    public boolean isLocked() {
-        return isLocked;
-    }
-
-    public void setLocked(boolean locked) {
-        isLocked = locked;
-    }
-
-    public boolean isCredentialsExpired() {
-        return isCredentialsExpired;
-    }
-
-    public void setCredentialsExpired(boolean credentialsExpired) {
-        isCredentialsExpired = credentialsExpired;
-    }
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        isEnabled = enabled;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
         this.roles = roles;
     }
 
